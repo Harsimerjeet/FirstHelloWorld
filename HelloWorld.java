43d01003152f9292e6fb87c46123121fb8d24e63
Harsimerjeet <32023335+Harsimerjeet@users.noreply.github.com>
2019-11-15 04:03:14 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-  public static void main (string[] args){
+  public static void main(String[] args){
      System.out.printIn("******** Hello Maniac Outside World**********");
 	 }
   }
